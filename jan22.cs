b0ad18fddeabdc8c01841910a3192245d65463ae
kramerica.eth <103228045+0xSamantha@users.noreply.github.com>
2024-01-23 11:08:33 -0800

    jan22

---
+++ jan22.cs
@@ -0,0 +1,11 @@
+string firstName = "Samantha";
+Console.WriteLine(firstName);
+
+var message = "Hello world!"; // var keyword tells compiler to assign data type based on assigned value
+// in the case above, the value is obviously a string and will be ran as such
+
+var name = "Bob";
+int num = 3;
+var num_2 = 34.4; 
+
+Console.Write(name + " " + num + " " + num_2); // to print all the following variables right after one another 
