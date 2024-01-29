af044c13385161b946ef98890a0f03f6d31cdf6b
kramerica.eth <103228045+0xSamantha@users.noreply.github.com>
2024-01-28 19:30:59 -0800

    boilerplate reference

---
+++ boilerplate-format.cs
@@ -0,0 +1,17 @@
+// dotnet new console -o ./CsharpProjects/TestProject in terminal
+// dotnet build
+// dotnet run
+using System;
+using System.Data; // if using Data
+
+class Program
+{
+    static void Main()
+    {
+        // Your code here
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
+
