dd23d57a1a8abdd9c865cd79622c1374417db8bd
kramerica.eth <103228045+0xSamantha@users.noreply.github.com>
2024-01-28 19:31:04 -0800

    idk

--- random.cs
+++ random.cs
@@ -1,1 +1,16 @@
-int
+using System;
+using System.Data;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine(Simple Calculator!);
+        Console.WriteLine(------------------);
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
+
+
